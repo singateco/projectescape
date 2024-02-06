b8da38554f5b30e559a6b57618b8913105f17001
Tenny <hhssyy0707@naver.com>
2024-02-06 18:49:59 +0900

    240206 SY PickupActor 생성

--- Source/ProjectEscape/ProjectEscape.h
+++ Source/ProjectEscape/ProjectEscape.h
@@ -3,3 +3,9 @@
 #pragma once
 
 #include "CoreMinimal.h"
+
+
+DECLARE_LOG_CATEGORY_EXTERN(SYLog, Log, All);
+
+#define  PRINTLOG(fmt, ... ) \
+UE_LOG(SYLog, Warning, TEXT("[%s]%s"), *CALLINFO, *FString::Printf(fmt, ##__VA_ARGS__))
