211ff62d208bc7d6cc98535c3f9811e751f291a4
Undoe <rgnjkl1222@gmail.com>
2024-02-07 23:54:59 +0900

    Ai 수정

---
+++ Source/ProjectEscape/Public/Enemy/EnemyAIPerception.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Perception/AIPerceptionComponent.h"
+#include "EnemyAIPerception.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTESCAPE_API UEnemyAIPerception : public UAIPerceptionComponent
+{
+	GENERATED_BODY()
+
+public:
+
+	UEnemyAIPerception();
+
+};
