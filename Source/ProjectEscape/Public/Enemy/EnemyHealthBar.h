2383115d2014393a5b90b85c4b07839d292a6a28
Undoe <rgnjkl1222@gmail.com>
2024-02-08 15:23:08 +0900

    적 체력바 생성

---
+++ Source/ProjectEscape/Public/Enemy/EnemyHealthBar.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "EnemyHealthBar.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTESCAPE_API UEnemyHealthBar : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+
+};
