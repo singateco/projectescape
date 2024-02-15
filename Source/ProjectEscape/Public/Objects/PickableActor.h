30da7c2944ad5255d410f271ef5530b66c86ef73
DESKTOP-1PQC82U\Seyun <hhssyy0707@gmail.com>
2024-02-15 15:44:04 +0900

    240215 sy 염력 스플래쉬 데미지, 에너미 데미지

--- Source/ProjectEscape/Public/Objects/PickableActor.h
+++ Source/ProjectEscape/Public/Objects/PickableActor.h
@@ -1,10 +1,15 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
 #include "PickableActor.generated.h"
+
+
+class UParticleSystem;
+class UStaticMeshComponent;
+class USphereComponent;
 
 UCLASS()
 class PROJECTESCAPE_API APickableActor : public AActor
@@ -15,6 +20,19 @@ public:
 	// Sets default values for this actor's properties
 	APickableActor();
 
+	UPROPERTY( EditAnywhere )
+	USphereComponent* CollisionComp;
+
+	UPROPERTY( EditAnywhere )
+	UStaticMeshComponent* MeshComp;
+
+	UPROPERTY( EditAnywhere )
+	float SphereRadius = 500.0f;
+
+	UPROPERTY( EditDefaultsOnly)
+	UParticleSystem* GunEffect;
+
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
@@ -23,4 +41,6 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+	UFUNCTION()
+	void OnCompHit( UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit );
 };
