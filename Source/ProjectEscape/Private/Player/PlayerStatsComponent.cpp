e560e9583134a35051e55fa8d93edf0265fc65e1
singateco <singateco@gmail.com>
2024-02-16 09:49:57 +0900

    업그레이드 제작중

--- Source/ProjectEscape/Private/Player/PlayerStatsComponent.cpp
+++ Source/ProjectEscape/Private/Player/PlayerStatsComponent.cpp
@@ -3,18 +3,64 @@
 
 #include "Player/PlayerStatsComponent.h"
 
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Player/ProjectEscapePlayer.h"
+#include "Player/Upgrade.h"
+#include "ProjectEscape/PEGameplayTags.h"
+
 
 // Sets default values for this component's properties
 UPlayerStatsComponent::UPlayerStatsComponent()
 {
 }
 
-float UPlayerStatsComponent::GetGunDamage()
+float UPlayerStatsComponent::GetGunDamage() const
 {
 	return GunDamage * GunDamageMultiplier;
+}
+
+void UPlayerStatsComponent::InitializeStat()
+{
+	GunDamage = InitialGunDamage;
+	GunDamageMultiplier = InitialGunDamageMultiplier;
+}
+
+
+void UPlayerStatsComponent::CalculateStat()
+{
+	
+	InitializeStat();
+	
+	for (UUpgrade* Upgrade : OwningUpgrades)
+	{
+		for (TTuple<FGameplayTag, float> Mod : Upgrade->StaticData.StatsToAlwaysModify)
+		{
+			if (Mod.Key.MatchesTagExact(PEGameplayTags::Stat_Gun_Damage))
+			{
+				GunDamage += Mod.Value;
+			}
+
+			if (Mod.Key.MatchesTagExact(PEGameplayTags::Stat_Gun_DamageMultiplier))
+			{
+				GunDamageMultiplier += Mod.Value;
+			}
+		}
+	}
 }
 
 void UPlayerStatsComponent::BeginPlay()
 {
 	Super::BeginPlay();
+	InitialGunDamage = GunDamage;
+	InitialGunDamageMultiplier = GunDamageMultiplier;
+
+	Player = GetOwner<AProjectEscapePlayer>();
+
+	TArray<FUpgradeData*> OutData;
+	UpgradeDataTable->GetAllRows(TEXT("Context"), OutData);
+
+	UUpgrade* Upgrade = NewObject<UUpgrade>(this, UUpgrade::StaticClass(), TEXT("Upgrade 1"));
+	Upgrade->StaticData = *OutData[0];
+	this->OwningUpgrades.Add(Upgrade);
+	CalculateStat();
 }
--- Source/ProjectEscape/Public/Player/PlayerStatsComponent.h
+++ Source/ProjectEscape/Public/Player/PlayerStatsComponent.h
@@ -7,6 +7,9 @@
 #include "PlayerStatsComponent.generated.h"
 
 
+class AProjectEscapePlayer;
+class UUpgrade;
+
 UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
 class PROJECTESCAPE_API UPlayerStatsComponent : public UStatsComponent
 {
@@ -15,18 +18,35 @@ class PROJECTESCAPE_API UPlayerStatsComponent : public UStatsComponent
 public:
 	// Sets default values for this component's properties
 	UPlayerStatsComponent();
+	
+	UFUNCTION()
+	float GetGunDamage() const;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	AProjectEscapePlayer* Player;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	TArray<UUpgrade*> OwningUpgrades;
 
 	UFUNCTION()
-	float GetGunDamage();
+	void CalculateStat();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UDataTable* UpgradeDataTable;
 
 protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
 
+	void InitializeStat();
+	
 private:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Damage", meta = (AllowPrivateAccess))
 	float GunDamage {1};
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Damage", meta = (AllowPrivateAccess))
 	float GunDamageMultiplier {1};
+	
+	float InitialGunDamage;
+	float InitialGunDamageMultiplier;
 };
