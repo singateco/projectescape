440b19b2febf6fb0f39a270808f5b74c42e32879
Undoe <rgnjkl1222@gmail.com>
2024-02-16 19:51:53 +0900

    수류탄 작업중

---
+++ Source/ProjectEscape/Public/Enemy/GrenadeEnemy.h
@@ -0,0 +1,20 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Enemy/EnemyBase.h"
+#include "GrenadeEnemy.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTESCAPE_API AGrenadeEnemy : public AEnemyBase
+{
+	GENERATED_BODY()
+
+public:
+	AGrenadeEnemy( const FObjectInitializer& ObjectInitializer );
+
+};
