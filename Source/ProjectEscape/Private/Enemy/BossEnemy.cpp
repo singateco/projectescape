d67bd427c0538e08f1ab70ef44c534c522d21285
Undoe <rgnjkl1222@gmail.com>
2024-02-24 23:57:48 +0900

    보스 체력바 구현중

---
+++ Source/ProjectEscape/Private/Enemy/BossEnemy.cpp
@@ -0,0 +1,13 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Enemy/BossEnemy.h"
+
+ABossEnemy::ABossEnemy(const FObjectInitializer& ObjectInitializer)
+	:
+	Super( ObjectInitializer )
+
+{
+	PrimaryActorTick.bCanEverTick=true;
+
+}
---
+++ Source/ProjectEscape/Public/Enemy/BossEnemy.h
@@ -0,0 +1,21 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Enemy/EnemyBase.h"
+#include "BossEnemy.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTESCAPE_API ABossEnemy : public AEnemyBase
+{
+	GENERATED_BODY()
+
+public:
+	ABossEnemy( const FObjectInitializer& ObjectInitializer );
+
+
+};
