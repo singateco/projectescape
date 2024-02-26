f0da9491f90b647cecee8fd15bbb7cd72dab8de7
Undoe <rgnjkl1222@gmail.com>
2024-02-26 23:46:58 +0900

    보스ai컨트롤러 추가중

--- Source/ProjectEscape/Private/Enemy/BossAIController.cpp
+++ Source/ProjectEscape/Private/Enemy/BossAIController.cpp
@@ -1,5 +1,10 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 
 #include "Enemy/BossAIController.h"
 
+ABossAIController::ABossAIController()
+{
+
+
+}
--- Source/ProjectEscape/Public/Enemy/BossAIController.h
+++ Source/ProjectEscape/Public/Enemy/BossAIController.h
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
@@ -13,5 +13,10 @@ UCLASS()
 class PROJECTESCAPE_API ABossAIController : public AAIController
 {
 	GENERATED_BODY()
-	
+
+
+public:
+	ABossAIController();
+
+
 };
